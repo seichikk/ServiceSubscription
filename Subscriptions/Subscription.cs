da79345fbf866d3d4639cd2a9a079219613f1080
seichikk <trchfk@gmail.com>
2026-08-16 12:36:18 +0300

    соблюдение инкапсуляции, заменив class на struct

---
+++ Subscriptions/Subscription.cs
@@ -0,0 +1,21 @@
+namespace SubscriptionService;
+
+public struct Subscription
+{
+    public SubscriptionType Type { get; private set; }
+    public bool HasBetaAccess { get; private set; }
+
+    public Subscription(SubscriptionType type)
+    {
+        Type = type;
+        
+        if (type == SubscriptionType.Premium)
+        {
+            HasBetaAccess = true;
+        }
+        else
+        {
+            HasBetaAccess = false;
+        }
+    }
+}
