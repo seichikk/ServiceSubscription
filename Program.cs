b596367af2a4edee255d448f6553d77919356897
seichikk <trchfk@gmail.com>
2026-08-16 12:43:20 +0300

    изменение точки входа с учетом айди

--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@ public class Program
         Subscription standardSubscription = new Subscription(SubscriptionType.Standard);
         Subscription premiumSubscription = new Subscription(SubscriptionType.Premium);
 
-        User user1 = new User("иван", freeSubscription);
-        User user2 = new User("олег", standardSubscription);
-        User user3 = new User("дима", premiumSubscription);
+        User user1 = new User("иван", freeSubscription, 1);
+        User user2 = new User("олег", standardSubscription, 2);
+        User user3 = new User("дима", premiumSubscription, 3);
 
         userStorage.Register(user1);
         userStorage.Register(user2);
